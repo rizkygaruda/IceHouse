3575355db0e8a5f6164f4a4b5307af453d85fa67
rizky.akbar <rizky.akbar@ottencoffee.co.id>
2022-10-18 00:01:09 +0700

    test update

---
+++ Object Repository/Page test/Page_Wikipedia/Suggestion Language.rs
@@ -0,0 +1,326 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Suggestion Language</name>
+   <tag></tag>
+   <elementGuidId>1fa8da36-edfb-4fd1-99d2-893d6d71946d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@id='searchLanguage']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#searchLanguage</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>searchLanguage</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>language</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+Afrikaans
+Polski
+العربية
+Asturianu
+Azərbaycanca
+Български
+Bân-lâm-gú / Hō-ló-oē
+বাংলা
+Беларуская
+Català
+Čeština
+Cymraeg
+Dansk
+Deutsch
+Eesti
+Ελληνικά
+English
+Español
+Esperanto
+Euskara
+فارسی
+Français
+Galego
+한국어
+Հայերեն
+हिन्दी
+Hrvatski
+Bahasa Indonesia
+Italiano
+עברית
+ქართული
+Latina
+Latviešu
+Lietuvių
+Magyar
+Македонски
+مصرى
+Bahasa Melayu
+Bahaso Minangkabau
+မြန်မာဘာသာ
+Nederlands
+日本語
+Norsk (bokmål)
+Norsk (nynorsk)
+Нохчийн
+Oʻzbekcha / Ўзбекча
+Português
+Қазақша / Qazaqşa / قازاقشا
+Română
+Simple English
+Sinugboanong Binisaya
+Slovenčina
+Slovenščina
+Српски / Srpski
+Srpskohrvatski / Српскохрватски
+Suomi
+Svenska
+தமிழ்
+Татарча / Tatarça
+ภาษาไทย
+Тоҷикӣ
+تۆرکجه
+Türkçe
+Українська
+اردو
+Tiếng Việt
+Volapük
+Winaray
+粵語
+中文
+Русский
+</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;searchLanguage&quot;)</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//select[@id='searchLanguage']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='search-input']/div/div/select</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='en'])[1]/following::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search Wikipedia'])[1]/following::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search'])[1]/preceding::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Read Wikipedia in your language'])[1]/preceding::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//select</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//select[@id = 'searchLanguage' and @name = 'language' and (text() = '
+Afrikaans
+Polski
+العربية
+Asturianu
+Azərbaycanca
+Български
+Bân-lâm-gú / Hō-ló-oē
+বাংলা
+Беларуская
+Català
+Čeština
+Cymraeg
+Dansk
+Deutsch
+Eesti
+Ελληνικά
+English
+Español
+Esperanto
+Euskara
+فارسی
+Français
+Galego
+한국어
+Հայերեն
+हिन्दी
+Hrvatski
+Bahasa Indonesia
+Italiano
+עברית
+ქართული
+Latina
+Latviešu
+Lietuvių
+Magyar
+Македонски
+مصرى
+Bahasa Melayu
+Bahaso Minangkabau
+မြန်မာဘာသာ
+Nederlands
+日本語
+Norsk (bokmål)
+Norsk (nynorsk)
+Нохчийн
+Oʻzbekcha / Ўзбекча
+Português
+Қазақша / Qazaqşa / قازاقشا
+Română
+Simple English
+Sinugboanong Binisaya
+Slovenčina
+Slovenščina
+Српски / Srpski
+Srpskohrvatski / Српскохрватски
+Suomi
+Svenska
+தமிழ்
+Татарча / Tatarça
+ภาษาไทย
+Тоҷикӣ
+تۆرکجه
+Türkçe
+Українська
+اردو
+Tiếng Việt
+Volapük
+Winaray
+粵語
+中文
+Русский
+' or . = '
+Afrikaans
+Polski
+العربية
+Asturianu
+Azərbaycanca
+Български
+Bân-lâm-gú / Hō-ló-oē
+বাংলা
+Беларуская
+Català
+Čeština
+Cymraeg
+Dansk
+Deutsch
+Eesti
+Ελληνικά
+English
+Español
+Esperanto
+Euskara
+فارسی
+Français
+Galego
+한국어
+Հայերեն
+हिन्दी
+Hrvatski
+Bahasa Indonesia
+Italiano
+עברית
+ქართული
+Latina
+Latviešu
+Lietuvių
+Magyar
+Македонски
+مصرى
+Bahasa Melayu
+Bahaso Minangkabau
+မြန်မာဘာသာ
+Nederlands
+日本語
+Norsk (bokmål)
+Norsk (nynorsk)
+Нохчийн
+Oʻzbekcha / Ўзбекча
+Português
+Қазақша / Qazaqşa / قازاقشا
+Română
+Simple English
+Sinugboanong Binisaya
+Slovenčina
+Slovenščina
+Српски / Srpski
+Srpskohrvatski / Српскохрватски
+Suomi
+Svenska
+தமிழ்
+Татарча / Tatarça
+ภาษาไทย
+Тоҷикӣ
+تۆرکجه
+Türkçe
+Українська
+اردو
+Tiếng Việt
+Volapük
+Winaray
+粵語
+中文
+Русский
+')]</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Page test/Page_Wikipedia/Suggestion Search.rs
@@ -0,0 +1,103 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Suggestion Search</name>
+   <tag></tag>
+   <elementGuidId>82d3c211-e0ae-4567-a72a-0c77c53a7533</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='typeahead-suggestions']/div/a/div/h3</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>h3.suggestion-title</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h3</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>suggestion-title</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Test automation</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;typeahead-suggestions&quot;)/div[@class=&quot;suggestions-dropdown&quot;]/a[@class=&quot;suggestion-link active&quot;]/div[@class=&quot;suggestion-text&quot;]/h3[@class=&quot;suggestion-title&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='typeahead-suggestions']/div/a/div/h3</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='en'])[1]/following::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Search Wikipedia'])[1]/following::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Test automation management tools'])[1]/preceding::h3[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Test Acts'])[1]/preceding::h3[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Test automation']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h3</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h3[(text() = 'Test automation' or . = 'Test automation')]</value>
+   </webElementXpaths>
+</WebElementEntity>
